95d95b29390ba14ee69480e9b4d14a65bf01297f
Charlie Samoel <charliesamoel@gmail.com>
2023-11-22 01:28:01 -0300

    update repository-url name

--- setup.py
+++ setup.py
@@ -17,5 +17,5 @@ setup(
     author='Charlie C.',
     author_email='charliesamoel@gmail.com',
     description='Esse pacote serve para calculo de derivadas',
-    url='https://github.com/CharlieCidral/deriv_calculus',
+    url='https://github.com/CharlieCidral/deriv-pypck',
 )
